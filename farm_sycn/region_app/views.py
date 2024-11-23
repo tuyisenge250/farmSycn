2d354cfc481b587b27d9c54927ad8ced6cb5dacd
tuyisenge250 <brnjaminwell250@gmail.com>
2024-11-23 22:53:53 +0200

    views

--- farm_sycn/region_app/views.py
+++ farm_sycn/region_app/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def Hello(request):
     return render(request, "landing.html")
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def overview(request):
+    return render(request, 'overview.html')
+
+def notification(request):
+    return render(request, 'notification.html')
+
+def complete_account(request):
+    return render(request, 'complete_account.html')
+
+def stock_mngt(request):
+    return render(request, 'stock_mngt.html')
+
+def login(request):
+    return render(request, 'login.html')
